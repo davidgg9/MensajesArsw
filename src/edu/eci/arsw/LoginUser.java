4d2777380b51b1154273e6ae8ad4dd9bd971f28b
dianaMahecha <diana.mahecha@mail.escuelaing.edu.co>
2013-03-13 20:25:25 -0500

    salvando loginUser

--- src/edu/eci/arsw/LoginUser.java
+++ src/edu/eci/arsw/LoginUser.java
@@ -15,29 +15,29 @@ public class LoginUser extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JTextField user;
 	private JButton login;
-	
+
 	public LoginUser(){
 		super();
 		user= new JTextField();
 		login = new JButton();
 		setSize(new Dimension(300,100));
-		
-		
+
+
 		login.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				new MainWindow(user.getText()).setVisible(true);
 				setVisible(false);
 				sendMessage();
-				
+
 			}
 		});
 	}
 
 	protected void sendMessage() {
-		
-		
+
+
 	}
 
 	/**
